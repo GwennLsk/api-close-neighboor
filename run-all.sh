ecfd9a0f4b83e0515078b062839723405cca4bff
Gwenn Linski <gwenn.linski@in-tact.fr>
2019-06-14 18:21:02 +0200

    modif script de lancement

--- run-all.sh
+++ run-all.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-rabbitmq-server >> ./logs/rabbitmq.txt &
-mongod >> ./logs/mongod.txt &
-sleep 10 &
+
 node ./apiGateway/app.js >> ./logs/apiGateway.txt &
 node ./UserService/app.js >> ./logs/UserService.txt &
 node ./StatutsServices/app.js >> ./logs/StatutsService.txt &
